4c192e5933653021368494810d9fbab47fcf6477
lemanols <emmanuel.aramendia@centraliens-nantes.net>
2017-04-08 11:54:23 +0200

    logo ensisa

--- assets/js/citations_aleatoires.js
+++ assets/js/citations_aleatoires.js
@@ -8,7 +8,7 @@ function aleatoire(N) {
 
 var listeCitations=['"Un moment privilégié pour s\'ouvrir aux opportunités, aux surprises et aux autres. Pour laisser les choses nous tomber dessus."',
 					'"J\'ai trouvé ça très sympathique même si on s\'est pris la pluie violemment pendant 1h30."',
-					'"C\'était la folie mais on a eut froid à 5h du mat à Amsterdam."',
+					'"C\'était la folie mais on a eu froid à 5h du mat à Amsterdam."',
 					'"Aventure exceptionnelle, avec plein de merveilleuses rencontres et des souvenirs gravés à jamais !"',
 					'"Superbe aventure, avec la présence rassurante d\'un staff organisé !"',
 					'"Comme l\'année dernière, un régal d\'émotions!"',
@@ -17,7 +17,7 @@ var listeCitations=['"Un moment privilégié pour s\'ouvrir aux opportunités, a
 					'"C\'était génial ! Juste un peu déçus d\'avoir été bloqués dans Lyon par la manif des motards.."',
 					'"A refaire l\'année prochaine, (en évitant de s\'engouffrer dans des routes paumées au milieu de la nuit ceci-dit)."',
 				    '"Et à partir de maintenant, je jure de prendre tout autostoppeur que je croise sur le bord de la route !"',
-				    '"Fou !!!!! Super experience, à refaire, et maintenant c\'est sur que je prendrai des gens en stop :)"',
+				    '"Fou !!!!! Super experience, à refaire, et maintenant c\'est sûr que je prendrai des gens en stop :)"',
 				    '"C\'était trop cool, sauf la petite nuit aux étoiles à Amsterdam."'];
 
 var listeAuteurs=['Stop\'épique',
